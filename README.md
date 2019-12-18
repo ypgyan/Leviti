bae96a37feabd96a75fad2bc6da2e164b4aa22d8
Yan de Paula <yan@impactaweb.com.br>
2019-12-18 15:45:52 -0300

    Inicializando projeto.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Leviti-API
+
+Projeto para gerência de membresia em igrejas.
+
+Utilizando:
+- Docker (https://docs.docker.com/)
+- Laravel Lumen (https://lumen.laravel.com/docs/6.x)
+- Swoole (https://www.swoole.co.uk/)
+- composer (https://getcomposer.org/)
+
+Comandos iniciais após clonar o repositório:
+- Composer install dentro da pasta leviti
+- php artisan migrate
+
+Obs: Os comandos devem ser rodados dentro do container do PHP caso esteja usando docker.
+
+Levantando o servidor
+
+Php:
+- php -S 0.0.0.0:3000 -t public
+
+Swoole:
+- php artisan swoole:http start
+-- O host e a porta usados pelo swoole estão no .env.example
+
+Obs: O comando deve ser rodado dentro da pasta api
---
+++ leviti/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->group(['namespace' => '\Rap2hpoutre\LaravelLogViewer'], function () use ($router) {
+    $router->get('logs', ['uses' => 'LogViewerController@index', 'name' => 'logs']);
+});
+
+$router->group(['prefix' => 'api', 'middleware' => ['auth', 'watchman']], function () use ($router) {
+    $router->get('/', ['uses' => '\Src\Gatekeeper\Controllers\IndexController@index', 'name' => 'api.index']);
+});
+
+$router->group(['prefix' => 'register', 'middleware' => ['watchman']], function () use ($router) {
+    $router->get('/', ['uses' => '\Src\Gatekeeper\Controllers\SignupController@index', 'name' => 'signup.index']);
+    $router->post('/signup', ['uses' => '\Src\Gatekeeper\Controllers\SignupController@signup', 'name' => 'signup.register']);
+});
